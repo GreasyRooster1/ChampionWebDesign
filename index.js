e518b028606247bfeb3c479df831e5f24213da7e
GreasyRooster <greasyRooster1@gmail.com>
2025-05-09 12:46:16 -0700

    light colors

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ function generateGridCSS(){
 }
 
 function getColor(){
-    let r = rand()*255;
-    let g = rand()*255;
-    let b = rand()*255;
+    let r = rand()*55+190;
+    let g = rand()*55+190;
+    let b = rand()*55+190;
     let tmp = r;
-    r*=1.89;
-    g*=2.3;
-    b*=1.8;
-    r+=10;
-    g+=30;
-    b+=50;
-    r = Math.min(r,220)
-    g = Math.max(g,100)
-    b-=Math.pow(tmp,2)/1000
+    // r*=1.89;
+    // g*=2.3;
+    // b*=1.8;
+    // r+=10;
+    // g+=30;
+    // b+=50;
+    // r = Math.min(r,220)
+    // g = Math.max(g,100)
+    // b-=Math.pow(tmp,2)/1000
     return "rgb("+r+", "+g+" , "+b+")";
 }
 
