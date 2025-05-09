10e46a8ad6743a2015050e1b231404a1b3b7e7bc
GreasyRooster <greasyRooster1@gmail.com>
2025-05-09 11:14:52 -0700

    change layout

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const divAmount = 57
 function generateGridCSS(){
     let str = "";
-    for(let i = 0; i < divAmount; i++){
-        str+="div"+i+"{background:"+getColor()+"}"
+    for(let i = 1; i <= divAmount; i++){
+        str+=".div"+i+"{background:"+getColor()+";}\n"
     }
+    let style = document.createElement("style");
+    style.innerHTML = str;
+    document.head.appendChild(style);
 }
 
 function getColor(){
-    return "#"+Math.floor(Math.random() * 0x10000).toString(16)
+    return "#"+Math.floor(Math.random() * 0x1000000).toString(16)
+}
+
+window.onload = ()=>{
+    generateGridCSS();
 }
